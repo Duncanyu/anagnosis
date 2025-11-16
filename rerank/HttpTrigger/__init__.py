0b499ee4ea9ccab947aac0edd04a4b4461d6fbb6
Duncanyu <duncan.w.yu@gmail.com>
2025-11-15 20:30:03 -0500

    Remove HTTPTrigger

--- rerank/HttpTrigger/__init__.py
+++
@@ -1,8 +0,0 @@
-import azure.functions as func
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    """Simple placeholder function for the rerank app.
-
-    Replace this with actual Azure Function logic that triggers your reranker worker/process.
-    """
-    return func.HttpResponse("rerank function placeholder", status_code=200)
